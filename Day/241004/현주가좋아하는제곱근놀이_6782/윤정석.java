feaac96726dabc8d2c539fadce63bdf0dfe3b6c4
Jay <108466918+IcarusToSun@users.noreply.github.com>
2024-10-02 07:43:17 +0900

    Update 윤정석.java

--- Day/241004/현주가좋아하는제곱근놀이_6782/윤정석.java
+++ Day/241004/현주가좋아하는제곱근놀이_6782/윤정석.java
@@ -5,28 +5,33 @@ package d5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
-public class D5_1256 {
+public class D5_6782 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		
 		int T = Integer.parseInt(br.readLine());
+		
 		for (int tc = 1; tc <= T; tc++) {
-			int K = Integer.parseInt(br.readLine());
-			String str = br.readLine();
-			String[] suffix = new String[str.length()];
-			suffix[0] = str;
-			suffix[str.length() - 1] = Character.toString(str.charAt(str.length() - 1));
+			long N = Long.parseLong(br.readLine());
 			
-			for (int i = str.length() - 2; i >= 1; i--) {
-				suffix[i] = str.charAt(i) + suffix[i + 1];
+			long cnt = 0;
+			if(N > 2) {
+				while(N != 2) {
+					long sq = (long) Math.sqrt(N);
+					if(sq*sq == N) {
+						cnt++;
+						N = sq;
+					} else {
+						long target = sq + 1;
+						cnt += target*target - N + 1;
+						N = target;
+					}
+				}
 			}
 			
-			Arrays.sort(suffix);
-			sb.append("#").append(tc).append(" ").append(suffix[K - 1] != null && K < str.length() ? suffix[K - 1] : "none").append("\n");
+			sb.append("#").append(tc).append(" ").append(cnt).append("\n");
 		}
 		System.out.println(sb);
 	}
