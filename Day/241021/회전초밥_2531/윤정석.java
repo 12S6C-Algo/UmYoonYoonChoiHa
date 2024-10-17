2bbd37d6141f2888fd090cbb91577d90286f603a
Jay <108466918+IcarusToSun@users.noreply.github.com>
2024-10-18 08:14:45 +0900

    Create 윤정석.java

---
+++ Day/241021/회전초밥_2531/윤정석.java
@@ -0,0 +1,85 @@
+// 24.10.18 Fri
+
+// 접시의 수 N, 초밥의 가짓수 d, 연속해서 먹는 접시의 수 k, 쿠폰 번호 c
+
+package problems;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_2531 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		
+		int[] dishes = new int[N];
+		boolean isCouponDish = false;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			dishes[i] = Integer.parseInt(st.nextToken());
+			if(dishes[i] == c) isCouponDish = true;
+		}
+		
+		
+		int[] cumul = new int[d + 1];
+		int left = 0, right = k, cnt = 0, result = 0;
+		
+		if(isCouponDish) {
+			for (int i = left; i < right; i++) {
+				cumul[dishes[i]]++;
+				if(cumul[dishes[i]] == 1) cnt++;
+			}
+			if(cumul[c] == 0) result = Math.max(result, cnt + 1);
+			else result = Math.max(result, cnt);
+			
+			while(true) {
+				if(cumul[dishes[left]] == 1) cnt--;
+				cumul[dishes[left]]--;
+				
+				cumul[dishes[right]]++;
+				if(cumul[dishes[right]] == 1) cnt++;
+				
+				if(cumul[c] == 0) result = Math.max(result, cnt + 1);
+				else result = Math.max(result, cnt);
+				
+				left++;
+				if(left == N) break;
+				right++;
+				if(right >= N) right %= N;
+			}
+		} else {
+			cnt = 1;
+			
+			for (int i = left; i < right; i++) {
+				cumul[dishes[i]]++;
+				if(cumul[dishes[i]] == 1) cnt++;
+			}
+			result = Math.max(result, cnt);
+			
+			while(true) {
+				if(cumul[dishes[left]] == 1) cnt--;
+				cumul[dishes[left]]--;
+				
+				cumul[dishes[right]]++;
+				if(cumul[dishes[right]] == 1) cnt++;
+				
+				result = Math.max(result, cnt);
+				
+				left++;
+				if(left == N) break;
+				right++;
+				if(right >= N) right %= N;
+			}
+		}
+		
+		System.out.println(result);
+	}
+
+}
