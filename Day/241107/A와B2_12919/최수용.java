bbbf4b293003b8a2611543bfb878ccce7dd6db58
choisuyong44 <121215043+choisuyong44@users.noreply.github.com>
2024-11-07 17:44:58 +0900

    Update 최수용.java

--- Day/241107/A와B2_12919/최수용.java
+++ Day/241107/A와B2_12919/최수용.java
@@ -1,40 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
 public class Main {
+	
+
+	static boolean flag = false;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine();
-		String target = br.readLine();
-		int targetSize = target.length();
+		String s = br.readLine();
+		String t = br.readLine();
+		dfs(s,t);
+		if(flag) System.out.println(1);
+		else System.out.println(0);
+	}
+	
+	public static void dfs(String s, String t) {
+		if(s.length()==t.length()) {
+			if(s.equals(t)) flag= true;
+			return;
+		}
 		
-		Stack<Character> st = new Stack<Character>();
-		for(int i =0;i<str.length();i++) {
-			st.push(str.charAt(i));
-			
-			if(st.size()>=targetSize) {
-				boolean flag = true;
-				
-				for(int j =0;j<targetSize;j++) {
-					if(st.get(st.size()-targetSize+j) != target.charAt(j)) {
-						flag = false;
-						break;
-					}
-				}
-				if(flag) {
-					for(int j=0;j<targetSize;j++) {
-						st.pop();
-					}
-				}
-			}
+		if(t.length()==0) return;
+		if(t.charAt(0)=='B') {
+			String sub = t.substring(1);
+			StringBuilder sb = new StringBuilder(sub);
+			String str = sb.reverse().toString();
+			dfs(s,str);
 		}
-		StringBuilder sb = new StringBuilder();
-		while(!st.isEmpty()) {
-			sb.append(st.pop());
+		
+		if(t.charAt(t.length()-1) =='A') {
+			dfs(s,t.substring(0, t.length()-1));
 		}
-		if(sb.length()==0)System.out.println("FRULA");
-		else System.out.println(sb.reverse().toString());
+		
+	
 	}
 }
