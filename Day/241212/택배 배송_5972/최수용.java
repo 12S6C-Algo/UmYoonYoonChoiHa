01e5accb779bdf909f3e7d3931a77bc3ac3853aa
choisuyong44 <121215043+choisuyong44@users.noreply.github.com>
2024-12-12 00:03:02 +0900

    Update 최수용.java

--- Day/241212/택배 배송_5972/최수용.java
+++ Day/241212/택배 배송_5972/최수용.java
@@ -1,32 +1,29 @@
+package algostudy;
+
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class P5972 {
 
-	static class Node implements Comparable<Node> {
-		int idx;
+	static class Node implements Comparable<Node>{
+		int dest;
 		int cost;
-		public Node(int idx,int cost) {
-			super();
-			this.idx = idx;
+		public Node(int dest, int cost) {
+			this.dest = dest;
 			this.cost = cost;
 		}
 		@Override
 		public int compareTo(Node o) {
-			// TODO Auto-generated method stub
 			return this.cost-o.cost;
 		}
-		@Override
-		public String toString() {
-			return "Node [idx=" + idx + ", cost=" + cost + "]";
-		}
 	}
-	static int INF = Integer.MAX_VALUE;
+	
+	static int N,M,A,B,W;
+	static List<Node>[] edge;
 	static int[] dist;
 	static boolean[] visited;
+	static int INF = Integer.MAX_VALUE;
 	static PriorityQueue<Node> pq = new PriorityQueue<Node>();
-	static List<Node>[] edge;
-	static int N,M,A,B,W;
 	
 	public static void main(String[] args) throws IOException{
 		input();
@@ -35,16 +32,16 @@ public class Main {
 	}
 	
 	public static void djikstra(int start) {
-		pq.add(new Node(start,0)); dist[start] =0;
+		pq.add(new Node(start,0)); dist[start] = 0;
 		while(!pq.isEmpty()) {
 			Node now = pq.poll();
-			if(visited[now.idx]) continue;
-            if(now.idx==N) return;
-			visited[now.idx]=true;
-			for(Node next : edge[now.idx]) {
-				if(dist[next.idx] > dist[now.idx]+next.cost) {
-					dist[next.idx] = dist[now.idx]+next.cost;
-					pq.add(new Node(next.idx,dist[next.idx]));
+			if(visited[now.dest]) continue;
+			if(now.dest ==N) return;
+			visited[now.dest] = true;
+			for(Node next: edge[now.dest]) {
+				if(dist[next.dest] > dist[now.dest]+next.cost) {
+					dist[next.dest] = dist[now.dest]+next.cost;
+					pq.add(new Node(next.dest,dist[next.dest]));
 				}
 			}
 		}
@@ -52,23 +49,25 @@ public class Main {
 	
 	public static void input() throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		st = new StringTokenizer(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
+		
 		edge = new List[N+1];
-		dist = new int[N+1]; Arrays.fill(dist, INF);
+		dist = new int[N+1];
 		visited = new boolean[N+1];
-		for(int i =0;i<=N;i++) {
-			edge[i] = new ArrayList<Node>();
+		for(int i =0;i<N+1;i++) {
+			edge[i] = new ArrayList<>();
+			dist[i] = Integer.MAX_VALUE;
 		}
-		for(int i =0;i<M;i++) {
+		
+		for(int i=0;i<M;i++) {
 			st = new StringTokenizer(br.readLine());
-			A = Integer.parseInt(st.nextToken());
-			B = Integer.parseInt(st.nextToken());
-			W = Integer.parseInt(st.nextToken());
-			edge[A].add(new Node(B,W));
-			edge[B].add(new Node(A,W));
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			edge[s].add(new Node(e,w));
+			edge[e].add(new Node(s,w));
 		}
 	}
 }
