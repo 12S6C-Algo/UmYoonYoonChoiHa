0905b205b6a9b567e123cbdaafa856e32a2bd966
choisuyong44 <121215043+choisuyong44@users.noreply.github.com>
2025-01-03 21:05:44 +0900

    Update 최수용.java

--- Programmers/250103_캐시/최수용.java
+++ Programmers/250103_캐시/최수용.java
@@ -2,18 +2,16 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-    static int size,time,idx;
+    static int size,time;
     static List<String> cache;
     public int solution(int cacheSize, String[] cities) {
         time = 0;
         size = cacheSize;
         cache = new ArrayList();
         
-        // cache size ==0
         if(cacheSize ==0){
             return 5*cities.length;
         }
-        
         for(int i =0;i<cities.length;i++){
             String city = cities[i].toLowerCase();
             insert(city);
@@ -24,15 +22,10 @@ class Solution {
     
     public static void insert(String city){
         if(!cache.contains(city)){
-            // cache 꽉찬 경우
             if(cache.size()==size){
                 cache.remove(0);
-                cache.add(city);
             }
-            // cache 꽉차지 않은 경우
-            else{
-                cache.add(city);    
-            }
+            cache.add(city);    
             time+=5;
         }
         
@@ -41,15 +34,5 @@ class Solution {
             cache.add(city);
             time++;
         }
-    }
-    
-    static boolean isSame(String a, String b){
-        int a_size = a.length();
-        int b_size = b.length();
-        if(a_size !=b_size) return false;
-        for(int i =0;i<a_size;i++){
-            if(a.charAt(i)!=b.charAt(i)) return false;    
-        }
-        return true;
     }
 }
