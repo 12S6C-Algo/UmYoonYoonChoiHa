48f457eb15097aed84520b8a64aaa8d6fa818358
choisuyong44 <121215043+choisuyong44@users.noreply.github.com>
2025-02-01 23:44:15 +0900

    Update 최수용.java

--- Day/250201/웜홀_1865/최수용.java
+++ Day/250201/웜홀_1865/최수용.java
@@ -1,54 +1,79 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+package study_aps;
 
-public class Main {
-    static int N;
-    static List<int[]>[] node;
-    static boolean[] visited;
-    static int ans;
+import java.io.*;
+import java.util.*;
+
+public class P1865 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int N,M,W,S,E,T;
+    static List<int[]>[] edge;
+    static int[] dist;
     public static void main(String[] args) throws IOException {
-        input();
-
-        ans = 0;
-        for(int i = 1; i <= N; i++){
-            visited = new boolean[N+1];
-            dfs(i,0);
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 0; tc < T; tc++) {
+            input();
+            if(hasMinusCycle())sb.append("YES").append("\n");
+            else sb.append("NO").append("\n");
         }
-
-        System.out.println(ans);
+        System.out.println(sb.toString());
     }
 
-    static void dfs(int idx, int l){
-        ans = Math.max(l,ans);
-        visited[idx] = true;
+    static boolean hasMinusCycle() {
+        Arrays.fill(dist, 0);
 
-        for(int[] n : node[idx]){
-            if(!visited[n[0]]){
-                dfs(n[0],l+n[1]);
+        // Edge 개수 : N-1
+        for (int i = 0; i < N - 1; i++) {
+            // 모든 Node 탐색
+            for (int now = 1; now <= N; now++) {
+                // 모든 노드의 이웃 노드 탐색
+                for (int[] next : edge[now]) {
+                    if (dist[next[0]] > dist[now] + next[1]) {
+                        dist[next[0]] = dist[now] + next[1];
+                    }
+                }
             }
         }
+
+        // 음수 사이클 탐지
+        for (int now = 1; now <= N; now++) {
+            for (int[] next : edge[now]) {
+                if (dist[next[0]] > dist[now] + next[1]) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
     static void input() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        node = new List[N + 1];
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        W = Integer.parseInt(st.nextToken());
+
+        dist = new int[N+1];
+        edge = new List[N+1];
         for (int i = 1; i <= N; i++) {
-            node[i] = new ArrayList();
+            edge[i] = new ArrayList<>();
         }
 
-        StringTokenizer st;
-        for(int i = 0; i < N-1; i++){
+        for(int i =0;i<M;i++){
             st = new StringTokenizer(br.readLine());
-            int p = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            int l = Integer.parseInt(st.nextToken());
+            S = Integer.parseInt(st.nextToken());
+            E = Integer.parseInt(st.nextToken());
+            T = Integer.parseInt(st.nextToken());
+            edge[S].add(new int[]{E,T});
+            edge[E].add(new int[]{S,T});
+        }
 
-            node[p].add(new int[]{c,l});
-            node[c].add(new int[]{p,l});
+        for(int i =0;i<W;i++){
+            st = new StringTokenizer(br.readLine());
+            S = Integer.parseInt(st.nextToken());
+            E = Integer.parseInt(st.nextToken());
+            T = Integer.parseInt(st.nextToken());
+            edge[S].add(new int[]{E,-T});
         }
     }
 }
