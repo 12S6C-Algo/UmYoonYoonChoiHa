5dd872accbb5ec66df9a754eb70da03f94cde9bb
엄윤준 <156382239+june2301@users.noreply.github.com>
2025-02-26 20:05:32 +0900

    Create 엄윤준.java

---
+++ Programmers/250226_행렬테두리회전하기/엄윤준.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+class Solution {
+    
+    int[][] map;
+    
+    public int[] solution(int rows, int columns, int[][] queries) {
+        map = new int[rows][columns];
+        int n = 1;
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < columns; c++) {
+                map[r][c] = n++;
+            }
+        }
+        
+        int[] ans = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            ans[i] = cycle(queries[i]);
+        }
+        
+        return ans;
+    }
+    
+    int cycle(int[] que) {
+        int sr = que[0]-1;
+        int sc = que[1]-1;
+        int er = que[2]-1;
+        int ec = que[3]-1;
+        
+        int rl = er-sr;
+        int cl = ec-sc;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        int save = map[sr][sc];
+        pq.add(save);
+        for (int i = 0; i < rl; i++) {
+            map[sr+i][sc] = map[sr+i+1][sc];
+            pq.add(map[sr+i][sc]);
+        }
+        for (int i = 0; i < cl; i++) {
+            map[er][sc+i] = map[er][sc+i+1];
+            pq.add(map[er][sc+i]);
+        }
+        for (int i = 0; i < rl; i++) {
+            map[er-i][ec] = map[er-i-1][ec];
+            pq.add(map[er-i][ec]);
+        }
+        for (int i = 0; i < cl; i++) {
+            map[sr][ec-i] = map[sr][ec-i-1];
+            pq.add(map[sr][ec-i]);
+        }
+        map[sr][sc+1] = save;
+        
+        return pq.poll();
+        
+    }
+    
+}
